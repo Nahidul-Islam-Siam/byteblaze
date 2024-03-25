3dd32415300fb67bc3e0c59dcdd89fd2f27c45e2
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-03-26 04:00:37 +0600

    90% done

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getBlogs } from "../utils";
 import BlogCard from "../Components/BlogCard";
 import { deleteBlog } from "../utils";
+import EmptyState from "../Components/EmptyState";
 
 const Bookmarks = () => {
 
@@ -17,6 +18,8 @@ setBlogs(storedBlogs)
         const storedBlogs = getBlogs()
 setBlogs(storedBlogs)
     }
+
+    if(blogs.length< 1) return <EmptyState message='No Bookmarks Available' address={'/blogs'} label={'Go to Blogs'}></EmptyState>
     return (
         <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 p-6">
 
